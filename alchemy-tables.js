c00b759304ba7096789bec8fd277a8ba8b984d12
Eric D Helms <ehelms@redhat.com>
2013-02-26 01:15:13 -0500

    Updating to use the new Alchemy component.

--- alchemy-tables.js
+++ alchemy-tables.js
@@ -1,8 +1,23 @@
+angular.module("alch-templates").run(function($templateCache) {
+  $templateCache.put("component/templates/table.html",
+    "<thead>" +
+    "  <tr>" +
+    "    <th ng-show=\"row_select\"><input ng-click=\"select_all(rows)\" class=\"select_all\" type=\"checkbox\" name=\"select_all\" value=\"none\"></td>" +
+    "    <th ng-repeat=\"header in table_data.row_headers\">{{ header }}</th>" +
+    "  </tr>" +
+    "</thead>" +
+    "<tbody>" +
+    "  <tr ng-repeat=\"row in table_data.rows\" ng-class=\"{active : row.selected }\">" +
+    "    <td ng-show=\"row_select\"><input ng-model=\"row.selected\" type=\"checkbox\" name=\"{{ row.id }}\" value=\"{{ row.id }}\"></td>" +
+    "    <td ng-repeat=\"cell in row.cells\">{{ cell }}</td>" +
+    "  </tr>" +
+    "</tbody>" +
+    "");
+});
+
 'use strict';
 
-var alchemy = angular.module('alchemy', []);
-    
-alchemy.directive('alchTable', function(){
+angular.module('alchemy').directive('alchTable', function(){
     return {
         restrict: 'A',
         transclude: true,
@@ -13,7 +28,7 @@ alchemy.directive('alchTable', function(){
     };
 });
 
-alchemy.directive('rowSelect', function(){
+angular.module('alchemy').directive('rowSelect', function(){
     return {
         require: 'alchTable',
 
@@ -33,21 +48,4 @@ alchemy.directive('rowSelect', function(){
             scope.row_select = true;
         }
     };
-});
-
-angular.module("component/templates/table.html", []).run(function($templateCache) {
-  $templateCache.put("component/templates/table.html",
-    "<thead>" +
-    "  <tr>" +
-    "    <th ng-show=\"row_select\"><input ng-click=\"select_all(rows)\" class=\"select_all\" type=\"checkbox\" name=\"select_all\" value=\"none\"></td>" +
-    "    <th ng-repeat=\"header in table_data.row_headers\">{{ header }}</th>" +
-    "  </tr>" +
-    "</thead>" +
-    "<tbody>" +
-    "  <tr ng-repeat=\"row in table_data.rows\" ng-class=\"{active : row.selected }\">" +
-    "    <td ng-show=\"row_select\"><input ng-model=\"row.selected\" type=\"checkbox\" name=\"{{ row.id }}\" value=\"{{ row.id }}\"></td>" +
-    "    <td ng-repeat=\"cell in row.cells\">{{ cell }}</td>" +
-    "  </tr>" +
-    "</tbody>" +
-    "");
 });
