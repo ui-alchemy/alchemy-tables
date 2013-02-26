3df4ff3940f3af97d9b27d0857450bdcbdb07a90
Eric D Helms <ehelms@redhat.com>
2013-02-25 23:12:17 -0500

    Updates Gruntfile to build alchemy-tables and include the SCSS, CSS and concatenated JS file.

---
+++ alchemy-tables.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var alchemy = angular.module('alchemy', []);
+    
+alchemy.directive('alchTable', function(){
+    return {
+        restrict: 'A',
+        transclude: true,
+        scope : true,
+        templateUrl: 'component/templates/table.html',
+
+        controller: function(){}
+    };
+});
+
+alchemy.directive('rowSelect', function(){
+    return {
+        require: 'alchTable',
+
+        controller : function($scope){
+            $scope.all_selected = false;
+
+            $scope.select_all = function(rows){
+                var selected = $scope.all_selected = !$scope.all_selected;
+
+                angular.forEach(rows, function(row){
+                    row.selected = selected;
+                });
+            };
+        },
+
+        link: function(scope){
+            scope.row_select = true;
+        }
+    };
+});
+
+angular.module("component/templates/table.html", []).run(function($templateCache) {
+  $templateCache.put("component/templates/table.html",
+    "<thead>" +
+    "  <tr>" +
+    "    <th ng-show=\"row_select\"><input ng-click=\"select_all(rows)\" class=\"select_all\" type=\"checkbox\" name=\"select_all\" value=\"none\"></td>" +
+    "    <th ng-repeat=\"header in table_data.row_headers\">{{ header }}</th>" +
+    "  </tr>" +
+    "</thead>" +
+    "<tbody>" +
+    "  <tr ng-repeat=\"row in table_data.rows\" ng-class=\"{active : row.selected }\">" +
+    "    <td ng-show=\"row_select\"><input ng-model=\"row.selected\" type=\"checkbox\" name=\"{{ row.id }}\" value=\"{{ row.id }}\"></td>" +
+    "    <td ng-repeat=\"cell in row.cells\">{{ cell }}</td>" +
+    "  </tr>" +
+    "</tbody>" +
+    "");
+});
