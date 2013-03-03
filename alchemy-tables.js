5fe988315c009570ff782f7bcc67e296682297e9
Eric D Helms <ehelms@redhat.com>
2013-03-03 08:41:47 -0500

    Adds testing and the sort functionality that can be called on a controller providing scope.  The styling present is simple and more for example purposes.

--- alchemy-tables.js
+++ alchemy-tables.js
@@ -2,14 +2,23 @@ angular.module("alch-templates").run(function($templateCache) {
   $templateCache.put("component/templates/table.html",
     "<thead>" +
     "  <tr>" +
-    "    <th ng-show=\"row_select\"><input ng-click=\"select_all(table_data.rows)\" class=\"select_all\" type=\"checkbox\" name=\"select_all\" value=\"none\"></td>" +
-    "    <th ng-repeat=\"header in table_data.row_headers\">{{ header }}</th>" +
+    "    <th ng-show=\"row_select\">" +
+    "      <input ng-click=\"select_all(rows)\" class=\"select_all\" type=\"checkbox\" name=\"select_all\" value=\"none\">" +
+    "    </th>" +
+    "    <th ng-click=\"sort(header)\" ng-show=\"header.show\" ng-repeat=\"header in columns\">" +
+    "      {{ header.display }}" +
+    "    </th>" +
     "  </tr>" +
     "</thead>" +
+    "" +
     "<tbody>" +
-    "  <tr ng-repeat=\"row in table_data.rows\" ng-class=\"{active : row.selected }\">" +
-    "    <td ng-show=\"row_select\"><input ng-model=\"row.selected\" type=\"checkbox\" name=\"{{ row.id }}\" value=\"{{ row.id }}\"></td>" +
-    "    <td ng-repeat=\"cell in row.cells\">{{ cell }}</td>" +
+    "  <tr ng-class=\"{active : row.selected }\" ng-repeat=\"row in rows\">" +
+    "    <td ng-show=\"row_select\">" +
+    "      <input ng-model=\"row.selected\" type=\"checkbox\" name=\"{{ row.id }}\" value=\"{{ row.id }}\">" +
+    "    </td>" +
+    "    <td ng-show=\"show_cell(cell)\" ng-repeat=\"cell in row.cells\">" +
+    "      {{ cell.display }}" +
+    "    </td>" +
     "  </tr>" +
     "</tbody>" +
     "");
@@ -21,10 +30,29 @@ angular.module('alchemy').directive('alchTable', function(){
     return {
         restrict: 'A',
         transclude: true,
-        scope : true,
+        scope: true,
         templateUrl: 'component/templates/table.html',
 
-        controller: function(){}
+        controller: function($scope){
+            $scope.show_cell = function(cell){
+                var to_show;
+
+                angular.forEach($scope.table_data.columns, function(header){
+                    if( header.id === cell.column_id ){
+                        to_show = header.show;
+                    }
+                });
+
+                return to_show;
+            };
+        },
+
+        link: function(scope, element, attrs){
+            scope.$watch(attrs.alchTable, function(data){
+                scope.rows = data.rows;
+                scope.columns = data.columns;
+            });
+        }
     };
 });
 
