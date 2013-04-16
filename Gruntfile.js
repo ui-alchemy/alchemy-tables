3c7786ef712f67fa512a6e403ac63705fa15db26
Eric D Helms <ehelms@redhat.com>
2013-04-16 10:55:55 -0400

    Updating how the CSS stylesheet is built.

--- Gruntfile.js
+++ Gruntfile.js
@@ -132,7 +132,7 @@ module.exports = function (grunt) {
             'component/scripts/**/*.js'
           ],
           'dist/<%= yeoman.component %>.css': [
-            '.tmp/styles/{,*/}*.css'
+            '.tmp/styles/main.css'
           ]
         }
       }
